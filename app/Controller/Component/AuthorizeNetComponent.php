68143205d61ef2456de261afe29c41ad2b441130
bhupeshg <bhupesh.gupta143@gmail.com>
2013-10-04 00:13:06 +0530

    resolve conflict

--- app/Controller/Component/AuthorizeNetComponent.php
+++ app/Controller/Component/AuthorizeNetComponent.php
@@ -1,6 +1,11 @@
+<<<<<<< HEAD
 <?php
 class AuthorizeNetComponent extends Component
 {
+=======
+<?php 
+class AuthorizeNetComponent extends Component {
+>>>>>>> 7fda71fbc79600e3186b4711bd43ee725e6d52ed
 
     ### Created By Graydon Stoner - www.getstonered.com ###
 
@@ -79,6 +84,7 @@ class AuthorizeNetComponent extends Component
 
     // class variables go here
 
+<<<<<<< HEAD
     public function startup(&$controller)
     {
         // This method takes a reference to the controller which is loading it.
@@ -160,10 +166,92 @@ class AuthorizeNetComponent extends Component
         curl_setopt($ch, CURLOPT_TIMEOUT, 120);
         ### End Go Daddy Specific CURL Options
 
+=======
+    public function startup(&$controller) {
+        // This method takes a reference to the controller which is loading it.
+        // Perform controller initialization here.
+    }
+    
+    public function chargeCard($url,$loginid, $trankey, $ccnum, $ccexpmonth, $ccexpyear, $ccver, $live, $amount, $tax, $shipping, $desc, $billinginfo, $email, $phone, $shippinginfo) {
+    
+        // setup variables
+        $ccexp = $ccexpmonth . '/' . $ccexpyear;
+        
+        $DEBUGGING                    = 1;                # Display additional information to track down problems
+        $TESTING                    = 1;                # Set the testing flag so that transactions are not live
+        $ERROR_RETRIES                = 2;                # Number of transactions to post if soft errors occur
+    
+        $auth_net_login_id            = $loginid;
+        $auth_net_tran_key            = $trankey;
+        ### $auth_net_url                = "https://certification.authorize.net/gateway/transact.dll";
+        #  Uncomment the line ABOVE for test accounts or BELOW for live merchant accounts
+        $auth_net_url                = $url;
+        
+        $authnet_values                = array
+        (
+            "x_login"                => $auth_net_login_id,
+            "x_version"                => "3.1",
+            "x_delim_char"            => "|",
+            "x_delim_data"            => "TRUE",
+            "x_url"                    => "FALSE",
+            "x_type"                => "AUTH_CAPTURE",
+            "x_method"                => "CC",
+            "x_tran_key"            => $auth_net_tran_key,
+            "x_relay_response"        => "FALSE",
+            "x_card_num"            => str_replace(" ", "", $ccnum),
+            "x_card_code"            => $ccver,
+            "x_exp_date"            => $ccexp,
+            "x_description"            => $desc,
+            "x_amount"                => $amount,
+            "x_tax"                    => $tax,
+            "x_freight"                => $shipping,
+            "x_first_name"            => $billinginfo["fname"],
+            "x_last_name"            => $billinginfo["lname"],
+            "x_address"                => $billinginfo["address"],
+            "x_city"                => $billinginfo["city"],
+            "x_state"                => $billinginfo["state"],
+            "x_zip"                    => $billinginfo["zip"],
+            "x_country"                => $billinginfo["country"],
+            "x_email"                => $email,
+            "x_phone"                => $phone,
+            "x_ship_to_first_name"    => $shippinginfo["fname"],
+            "x_ship_to_last_name"    => $shippinginfo["lname"],
+            "x_ship_to_address"        => $shippinginfo["address"],
+            "x_ship_to_city"        => $shippinginfo["city"],
+            "x_ship_to_state"        => $shippinginfo["state"],
+            "x_ship_to_zip"            => $shippinginfo["zip"],
+            "x_ship_to_country"        => $shippinginfo["country"],
+        );
+        
+        $fields = "";
+        foreach ( $authnet_values as $key => $value ) $fields .= "$key=" . urlencode( $value ) . "&";
+        
+        ///////////////////////////////////////////////////////////
+        
+        // Post the transaction (see the code for specific information)
+        
+        
+        ### $ch = curl_init("https://certification.authorize.net/gateway/transact.dll");
+        ###  Uncomment the line ABOVE for test accounts or BELOW for live merchant accounts
+        $ch = curl_init($auth_net_url);  
+        ### curl_setopt($ch, CURLOPT_URL, "https://secure.authorize.net/gateway/transact.dll");
+        curl_setopt($ch, CURLOPT_HEADER, 0); // set to 0 to eliminate header info from response
+        curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1); // Returns response data instead of TRUE(1)
+        curl_setopt($ch, CURLOPT_POSTFIELDS, rtrim( $fields, "& " )); // use HTTP POST to send form data
+        
+        ### Go Daddy Specific CURL Options
+        //curl_setopt($ch, CURLOPT_HTTPPROXYTUNNEL, true); 
+        //curl_setopt($ch, CURLOPT_PROXYTYPE, CURLPROXY_HTTP); 
+           //curl_setopt($ch, CURLOPT_PROXY, 'http://proxy.shr.secureserver.net:3128'); 
+        curl_setopt($ch, CURLOPT_TIMEOUT, 120);
+           ### End Go Daddy Specific CURL Options
+           
+>>>>>>> 7fda71fbc79600e3186b4711bd43ee725e6d52ed
         curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, FALSE); // uncomment this line if you get no gateway response. ###
         $resp = curl_exec($ch); //execute post and get results
         $output = array();
         if (curl_error($ch)) {
+<<<<<<< HEAD
             // If it wasn't...
             $output['response']['code'] = '-1';
             $output['response']['responseMsg'] = curl_error($ch);
@@ -236,6 +324,84 @@ class AuthorizeNetComponent extends Component
         return $output;
 
 
+=======
+                // If it wasn't...
+                $output['response']['code'] = '-1';
+                $output['response']['responseMsg'] = curl_error($ch);
+        } else {
+                $text = $resp;
+                $h = substr_count($text, "|");
+                $h++;
+                $responsearray = array();
+        
+                for($j=1; $j <= $h; $j++){
+        
+                    $p = strpos($text, "|");
+        
+                    if ($p === false) { // note: three equal signs
+                        //  x_delim_char is obviously not found in the last go-around
+                        // This is final response string
+                        $responsearray[$j] = $text;
+                    }
+                    else {
+                        $p++;
+                        //  get one portion of the response at a time
+                        $pstr = substr($text, 0, $p);
+        
+                        //  this prepares the text and returns one value of the submitted
+                        //  and processed name/value pairs at a time
+                        //  for AIM-specific interpretations of the responses
+                        //  please consult the AIM Guide and look up
+                        //  the section called Gateway Response API
+                        $pstr_trimmed = substr($pstr, 0, -1); // removes "|" at the end
+        
+                        if($pstr_trimmed==""){
+                            $pstr_trimmed="";
+                        }
+        
+                        $responsearray[$j] = $pstr_trimmed;
+        
+                        // remove the part that we identified and work with the rest of the string
+                        $text = substr($text, $p);
+        
+                    } // end if $p === false
+        
+                } // end parsing for loop
+                
+                if($responsearray[1] == 1){
+                    $output['response']['code'] = 100;
+                    $output['response']['transaction_id'] = $responsearray[7];
+                    $output['response']['md5_hash'] = $responsearray[38];
+                    $output['response']['amount'] = $responsearray[10];
+                    
+                }
+                else{
+                    $output['response']['code'] = $responsearray[1];
+                    $output['response']['reason_code'] = $responsearray[2];
+                    $output['response']['message'] = $responsearray[4];
+                }
+                //$responsearray[1] = Response Code (1 = Approved, 2 = Declined, 3 = Error, 4 = Held for Review)
+                //$responsearray[2] = Response Subcode (Code used for Internal Transaction Details)
+                //$responsearray[3] = Response Reason Code (Code detailing response code)
+                //$responsearray[4] = Response Reason Text (Text detailing response code and response reason code)
+                //$responsearray[5] = Authorization Code (Authorization or approval code - 6 characters)
+                //$responsearray[6] = AVS Response (Address Verification Service response code - A, B, E, G, N, P, R, S, U, W, X, Y, Z)
+                //                (A, P, W, X, Y, Z are default AVS confirmation settings - Use your Authorize.net Merchant Interface to change these settings)
+                //                (B, E, G, N, R, S, U are default AVS rejection settings - Use your Authorize.net Merchant Interface to change these settings)
+                //$responsearray[7] = Transaction ID (Gateway assigned id number for the transaction)
+                //$responsearray[38] = MD5 Hash (Gateway generated MD5 has used to authenticate transaction response)
+                //$responsearray[39] = Card Code Response (CCV Card Code Verification response code - M = Match, N = No Match, P = No Processed, S = Should have been present, U = Issuer unable to process request)      
+        }
+        
+        curl_close ($ch);
+        
+        
+        
+        
+        
+        return $output;
+        
+>>>>>>> 7fda71fbc79600e3186b4711bd43ee725e6d52ed
     } // end chargeCard function
 }
 ?> 
